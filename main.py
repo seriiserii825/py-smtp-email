a07aa83b768e0526c04b936a051d47cbc8bdce42
serii burduja <seriiburduja@gmail.com>
2024-10-17 18:13:04 +0300

    feat: quotes txt

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-import os
+# import os
+#
+# command = f"mutt -s 'Subject here' seriiburduja@gmail.com < form.txt"
+# os.system(command)
 
-command = f"mutt -s 'Subject here' seriiburduja@gmail.com < form.txt"
-os.system(command)
+import datetime as dt
+
+now = dt.datetime.now()
+print(f"now: {now}")
+print(f"now.year: {now.year}")
+print(f"now.month: {now.month}")
+print(f"now.day: {now.day}")
+print(f"now.hour: {now.hour}")
+print(f"now.minute: {now.minute}")
+print(f"now.second: {now.second}")
+print(f"now.date(): {now.date()}")
+print(f"now.time(): {now.time()}")
+
+birthday = dt.datetime(year=1990, month=1, day=1)
+print(f"birthday: {birthday}")
+
+
