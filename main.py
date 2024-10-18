3aa803de769a0f3c5cbc531176663e6355f840bd
serii burduja <seriiburduja@gmail.com>
2024-10-18 09:01:20 +0300

    feat: small change

--- main.py
+++ main.py
@@ -1,50 +1,44 @@
 import os
 import random
 import pandas
-
 import datetime as dt
 
-now = dt.datetime.now()
-# print(f"now: {now}")
-# print(f"now.year: {now.year}")
-# print(f"now.month: {now.month}")
-# print(f"now.day: {now.day}")
-# print(f"now.hour: {now.hour}")
-# print(f"now.minute: {now.minute}")
-# print(f"now.second: {now.second}")
-# print(f"now.date(): {now.date()}")
-# print(f"now.time(): {now.time()}")
-#
-# birthday = dt.datetime(year=1990, month=1, day=1)
-# print(f"birthday: {birthday}")
-
-# with open('quotes.txt') as file:
-#     quotes = file.readlines()
-#
-# quote = random.choice(quotes)
-# with open('form.txt', 'w') as file:
-#     file.write(quote)
-#
-# current_weekday = now.weekday()
-#
-# week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-# current_weekday = week_days[current_weekday]
-
-current_month = now.month
-current_day = now.day
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
-all_birthdays = data.to_dict(orient="records")
+birthdays_dict = {(row.month, row.day): row for (index, row) in data.iterrows()}
 
-for birthday in all_birthdays:
-    if birthday["month"] == current_month and birthday["day"] == current_day:
-        name = birthday['name']
-        email = birthday['email']
-        with open('letter.txt') as file:
-            letter = file.read()
-            letter = letter.replace("[NAME]", name)
-        with open('form.txt', 'w') as file:
-            file.write(letter)
-        command = f"mutt -s 'Subject ' {email} < form.txt"
-        os.system(command)
+if today_tuple in birthdays_dict:
+    birthday = birthdays_dict[today_tuple]
+    name = birthday['name']
+    email = birthday['email']
+    with open('letter.txt') as file:
+        letter = file.read()
+        letter = letter.replace("[NAME]", name)
+    with open('form.txt', 'w') as file:
+        file.write(letter)
+    command = f"mutt -s 'Subject ' {email} < form.txt"
+    os.system(command)
 
+# import datetime as dt
+#
+# now = dt.datetime.now()
+# current_month = now.month
+# current_day = now.day
+#
+# data = pandas.read_csv("birthdays.csv")
+# all_birthdays = data.to_dict(orient="records")
+#
+# for birthday in all_birthdays:
+#     if birthday["month"] == current_month and birthday["day"] == current_day:
+#         name = birthday['name']
+#         email = birthday['email']
+#         with open('letter.txt') as file:
+#             letter = file.read()
+#             letter = letter.replace("[NAME]", name)
+#         with open('form.txt', 'w') as file:
+#             file.write(letter)
+#         command = f"mutt -s 'Subject ' {email} < form.txt"
+#         os.system(command)
+#
