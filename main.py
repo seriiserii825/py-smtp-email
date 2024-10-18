b9ccc452276a50b6b5f0271d961c0720a8d9acb6
serii burduja <seriiburduja@gmail.com>
2024-10-18 10:09:01 +0300

    feat: smtp update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-import random
 import pandas
 import datetime as dt
+import smtplib
 
 today = dt.datetime.now()
 today_tuple = (today.month, today.day)
@@ -21,24 +21,12 @@ if today_tuple in birthdays_dict:
     command = f"mutt -s 'Subject ' {email} < form.txt"
     os.system(command)
 
-# import datetime as dt
-#
-# now = dt.datetime.now()
-# current_month = now.month
-# current_day = now.day
-#
-# data = pandas.read_csv("birthdays.csv")
-# all_birthdays = data.to_dict(orient="records")
-#
-# for birthday in all_birthdays:
-#     if birthday["month"] == current_month and birthday["day"] == current_day:
-#         name = birthday['name']
-#         email = birthday['email']
-#         with open('letter.txt') as file:
-#             letter = file.read()
-#             letter = letter.replace("[NAME]", name)
-#         with open('form.txt', 'w') as file:
-#             file.write(letter)
-#         command = f"mutt -s 'Subject ' {email} < form.txt"
-#         os.system(command)
-#
+my_email = "seriiburduja@gmail.com"
+password = "" #password from google app https://myaccount.google.com/apppasswords
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="seriiburduja@yandex.ru", msg="Subject:Hello\n\nThis is the body of my email")
+connection.close()
+
+
